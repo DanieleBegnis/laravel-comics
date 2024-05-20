684ce1aff74774bd6b6a9f7282279c7e32ed7706
DanieleBegnis <daniele.begnis@gmail.com>
2024-05-20 16:43:55 +0200

    feat: add home, app , header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,44 @@
+<header>
+    <div class="container d-flex ">
+        <div class="row">
+            <div class="col-6">
+                <img class="py-2" src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+            </div>
+            <div class="col-6 d-flex align-items-center py-3">
+                <ul class="d-flex">
+                    <li>
+                        <a href="#">Characters</a>
+                    </li>
+                    <li>
+                        <a href="#">Comics</a>
+                    </li>
+                    <li>
+                        <a href="#">Movies</a>
+                    </li>
+                    <li>
+                        <a href="#">Tv</a>
+                    </li>
+                    <li>
+                        <a href="#">Games</a>
+                    </li>
+                    <li>
+                        <a href="#">Collectibles</a>
+                    </li>
+                    <li>
+                        <a href="#">Videos</a>
+                    </li>
+                    <li>
+                        <a href="#">Fans</a>
+                    </li>
+                    <li>
+                        <a href="#">News</a>
+                    </li>
+                    <li>
+                        <a href="#">Shop</a>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
