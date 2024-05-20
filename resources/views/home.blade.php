cd8c302f0db81b6a09d360028ef96bb792b4f0d7
DanieleBegnis <daniele.begnis@gmail.com>
2024-05-20 17:19:59 +0200

    feat: ad dmain links container

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,6 +21,35 @@
         <div class="py-3 d-flex justify-content-center">
             <div class="box p-3 fs-6 font-weight-bold">LOAD MORE</div>
         </div>
-
+        <div class="main-links-container d-flex">
+            <div class="container d-flex ">
+                <div class="row">
+                    <div class="col-12 d-flex align-items-center" >
+                        <ul class="d-flex py-5">
+                            <li class="px-3 d-flex align-items-center fs-6"">
+                                <img class="mx-3" src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="">
+                                <a class="px-6" href="#">DIGITAL COMICS</a>
+                            </li>
+                            <li class="px-3 d-flex align-items-center fs-6"">
+                                <img class="mx-3" src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="">
+                                <a class="px-6" href="#">DC MERCHANDISE</a>
+                            </li>
+                            <li class="px-3 d-flex align-items-center fs-6"">
+                                <img class="mx-3" src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="">
+                                <a class="px-6" href="#">SUBSCRIPTION</a>
+                            </li>
+                            <li class="px-3 d-flex align-items-center fs-6"">
+                                <img class="mx-3" src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="">
+                                <a class="px-6" href="#">COMIC SHOP LOCATOR</a>
+                            </li>
+                            <li class="px-3 d-flex align-items-center fs-6"">
+                                <img class="mx-3" src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="">
+                                <a class="px-6" href="#">DC POWER VISA</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
 @endsection
